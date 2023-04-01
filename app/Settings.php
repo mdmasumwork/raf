60cabbde8e9ee99e3f9e7bc21fc1b4dd28bd1ab1
Md Masum <masum@talentedcube.com>
2023-03-31 23:29:24 -0500

    working on search result page

--- app/Settings.php
+++ app/Settings.php
@@ -9,6 +9,17 @@ class Settings
         add_action('init', [get_called_class(), 'initForSettings']);
         add_action('pre_get_posts', [get_called_class(), 'posts_for_current_author']);
         add_filter('ajax_query_attachments_args', [get_called_class(), 'wpb_show_current_user_attachments']);
+        add_filter('excerpt_length', [get_called_class(), 'raf_custom_excerpt_length'], 999);
+        add_filter('excerpt_more', [get_called_class(), 'custom_excerpt_more'], 21);
+    }
+    
+    public static function custom_excerpt_more($more) {
+        global $post;
+        return ' ... <a href="' . get_permalink($post->ID) . '">Read more</a>';
+    }
+    
+    public static function raf_custom_excerpt_length($length) {
+        return 30;
     }
     
     public static function initForSettings()
@@ -22,7 +33,7 @@ class Settings
                     'name' => 'Services',
                     'singular_name' => 'Service'
                 ),
-                'supports' => array('title', 'editor', 'thumbnail', 'excerpt', 'comments'),
+                'supports' => array('title', 'editor', 'thumbnail', 'comments'),
                 'public' => true,
                 'has_archive' => true,
                 'rewrite' => array('slug' => 'services')
--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -12,8 +12,9 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('card'); ?>>
     <?php roadside_auto_fix_post_thumbnail(); ?>
     <div class="card-content">
-        <h2><?= the_title() ?></h2>
-        <p><?= the_excerpt() ?></p>
+        <h2><a href="<?= get_the_permalink() ?>"><?= the_title() ?></a></h2>
+        <?= the_excerpt() ?>
         <h3>$<?= get_field('price') ?></h3>
+        <p>No rating yet</p>
     </div>
 </article><!-- #post-<?php the_ID(); ?> -->
